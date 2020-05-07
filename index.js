d19e6881b1d89a76c626243b63a7159d654844fe
Yoni Calsin <yonicb20@gmail.com>
2020-05-07 13:19:46 -0500

    refactor: 💡 Builded to es5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const is_all_utils_1 = require("is-all-utils");
-const formatFast = (source, replaces, iterateFn, brackets) => {
+var is_all_utils_1 = require("is-all-utils");
+var formatFast = function (source, replaces, iterateFn, brackets) {
     if (is_all_utils_1.isUndefined(replaces))
         return source;
     !is_all_utils_1.isObj(replaces) && (replaces = [replaces]);
@@ -9,12 +9,15 @@ const formatFast = (source, replaces, iterateFn, brackets) => {
         brackets = iterateFn;
         iterateFn = undefined;
     }
-    let getKey = (v) => is_all_utils_1.isString(brackets) ? brackets.replace(/%s/g, v) : `$${v}`;
-    for (const [key, value] of Object.entries(replaces)) {
-        let newKey = getKey(key);
-        let newValue = value;
+    var getKey = function (v) {
+        return is_all_utils_1.isString(brackets) ? brackets.replace(/%s/g, v) : "$" + v;
+    };
+    for (var _i = 0, _a = Object.entries(replaces); _i < _a.length; _i++) {
+        var _b = _a[_i], key = _b[0], value = _b[1];
+        var newKey = getKey(key);
+        var newValue = value;
         if (is_all_utils_1.isFunction(iterateFn)) {
-            const dataIterate = iterateFn(value, key);
+            var dataIterate = iterateFn(value, key);
             if (is_all_utils_1.isArray(dataIterate) && dataIterate.length === 2) {
                 newKey = getKey(dataIterate[1]);
                 newValue = dataIterate[0];
